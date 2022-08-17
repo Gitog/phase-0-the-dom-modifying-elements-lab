91be54e33a84a733be941ed969e1c660e2e761e9
James <jamesnjorio@gmail.com>
2022-08-17 20:49:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove()
+
+var newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+newHeader.textContent = 'James Njorio is the champion'
